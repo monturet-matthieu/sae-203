8e3c93bb782eeee1499f173ab59e97cda4c5706b
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2022-05-30 22:28:44 +0200

    création tableau programmation

--- src/views/Programmation.vue
+++ src/views/Programmation.vue
@@ -1,3 +1,87 @@
 <template>
-
+    <p class="text-montserrat text-lg mx-64 mt-12 mb-3 text-center">Voici le planning complet de Blue Jazz !</p>
+    <p class="text-montserrat text-lg mx-64 mb-12 text-left">Vous retrouverez les artistes présents à notre festival, certains faisant plusieurs apparitions sur la semaine</p>
+    <table class="w-full text-center mb-8 border-collapse font-montserrat ">
+        <caption class="sr-only">Planning des différents passages d'artistes sur la durée du festival</caption>
+            <colgroup>
+            <col class="border-2">
+            <col class="border-2">
+            <col class="border-2">
+            <col class="border-2">
+            <col class="border-2">
+            <col class="border-2">
+            </colgroup>
+        <thead class="border-4 border-b-2 bg-blue-850">
+            <tr>
+                <th class="py-4 text-2xl font-extrabold font-paytone" colspan="7">Programmation</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-4 border-b-2">
+                <td class="py-2" scope="col">Horaires</td>
+                <td class="px-2" scope="col">10 juillet</td>
+                <td class="px-2" scope="col">11 juillet</td>
+                <td class="px-2" scope="col">12 juillet</td>
+                <td class="px-2" scope="col">13 juillet</td>
+                <td class="px-2" scope="col">14 juillet</td>
+                <td class="px-2" scope="col">15 juillet</td>
+            </tr>
+            <tr class="border-2 border-x-4">
+                <td class="py-16" scope="row">19h-20h</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr class="border-2 border-x-4">
+                <td class="py-16" scope="row">20h-21h</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr class="border-2 border-x-4">
+                <td class="py-16" scope="row">21h-22h</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr class="border-2 border-x-4">
+                <td class="py-16" scope="row">22h-23h</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr class="border-2 border-x-4">
+                <td class="py-16" scope="row">23h-0h</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+        <tfoot class="border-b-4 border-x-4">
+            <tr class="border-2">
+                <td class="py-16" scope="row">0h-...</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tfoot>
+    </table>
 </template>
