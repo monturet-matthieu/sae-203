74765b81e59b869892162a78c859024585f636eb
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2022-05-31 15:30:42 +0200

    Programmation

--- src/views/Programmation.vue
+++ src/views/Programmation.vue
@@ -1,7 +1,10 @@
 <template>
-    <p class="text-montserrat text-lg mx-64 mt-12 mb-3 text-center">Voici le planning complet de Blue Jazz !</p>
-    <p class="text-montserrat text-lg mx-64 mb-12 text-left">Vous retrouverez les artistes présents à notre festival, certains faisant plusieurs apparitions sur la semaine</p>
-    <table class="w-full text-center mb-8 border-collapse font-montserrat ">
+    <h2 class="font-gothic font-bold text-2xl text-center my-12">Programmation</h2>
+
+    <p class="text-montserrat text-lg xl:mx-64 lg:mx-32 md:16 sm:mx-8 mt-12 text-center">Voici le planning complet de Blue Jazz !</p>
+    <p class="text-montserrat text-lg xl:mx-64 lg:mx-32 md:16 sm:mx-8 mb-12 text-center">Vous retrouverez les artistes présents à notre festival, la plupart faisant plusieurs apparitions dans la semaine. Ce planning peut être sujet à des changements de dernière minute, pensez à le consulter régulièrement !</p>
+    
+    <table class="w-full table-fixed text-center mb-8 border-collapse font-montserrat text-sm font-medium md:text-base lg:text-lg lg:font-semibold">
         <caption class="sr-only">Planning des différents passages d'artistes sur la durée du festival</caption>
             <colgroup>
             <col class="border-2">
@@ -28,60 +31,67 @@
             </tr>
             <tr class="border-2 border-x-4">
                 <td class="py-16" scope="row">19h-20h</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>Stabar</td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>Emile Parisien</td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>NCY Milky Band</td>
             </tr>
             <tr class="border-2 border-x-4">
                 <td class="py-16" scope="row">20h-21h</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>Les Mains Sales</td>
+                <td>La Récré</td>
+                <td>Maxime Bender Quartet</td>
+                <td>Les Mains Sales</td>
+                <td>Gabriel Vallejo</td>
             </tr>
             <tr class="border-2 border-x-4">
                 <td class="py-16" scope="row">21h-22h</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>Les Gitanes Maïs</td>
+                <td>Baptiste Herbin</td>
+                <td>NCY Milky Band</td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>Baptiste Herbin</td>
+                <td>Orchestre Régional de Jazz Alsace</td>
             </tr>
             <tr class="border-2 border-x-4">
                 <td class="py-16" scope="row">22h-23h</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>Emile Parisien</td>
+                <td>Maxime Bender Quartet</td>
+                <td>Denis Badault</td>
+                <td>Ibrahim Maalouf</td>
+                <td>Gabriel Vallejo</td>
+                <td>Orchestre Régional de Jazz Alsace</td>
             </tr>
             <tr class="border-2 border-x-4">
                 <td class="py-16" scope="row">23h-0h</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>NCY Milky Band</td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>Les Gitanes Maïs</td>
+                <td>Ibrahim Maalouf</td>
+                <td>Les Gitanes Maïs</td>
+                <td>Orchestre Régional de Jazz Alsace</td>
             </tr>
         </tbody>
         <tfoot class="border-b-4 border-x-4">
             <tr class="border-2">
                 <td class="py-16" scope="row">0h-...</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td>Denis Badault</td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>Stabar</td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
+                <td>La Récré</td>
+                <td class="flex justify-center mt-6"><XIcon class="w-24 h-24" /></td>
             </tr>
         </tfoot>
     </table>
 </template>
+
+<script>
+import { XIcon } from '@heroicons/vue/outline'
+export default {
+  components: { XIcon }
+}
+</script>
