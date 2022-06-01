5d8f6a65c6f25475d679dd68f67e40c682731236
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2022-06-01 15:42:00 +0200

    festival

--- src/views/Festival.vue
+++ src/views/Festival.vue
@@ -1,3 +1,24 @@
 <template>
-    
+    <h2 class="font-gothic font-bold text-2xl text-center my-12 sm:mb-6">À propos du festival</h2>
+
+    <div class="flex flex-col font-montserrat font-medium text-left md:text-center lg:text-left mx-4 md:mx-12 lg:mx-28 lg:mr-64 xl:mr-96 mb-8">
+        <div class="mb-12 space-y-2">
+            <h4 class="font-semibold text-base lg:text-lg pt-8 pb-6 lg:text-left">Déroulement du festival</h4>
+            <p class="text-sm lg:text-base">Le festival sera présent 6 jours, en s'étendant du 10 au 16 juillet. Chaque personne souhaitant assister à une ou plusieurs journées du festival pourront avoir accès à une billetterie située à l'entrée du festival. Plusieurs formules d'entrée seront disponible tout au long du festival (voir rubrique "Pass et tarifs").</p>
+        
+            <h4 class="font-semibold text-base lg:text-lg pt-8 pb-6 lg:text-left">Emplacement du festival</h4>
+            <p class="text-sm lg:text-base">Le festival aura lieu devant la Commanderie de Dole, France (39100). Voici une carte situant l'endroit exact et ses environs.</p>
+            <a class="flex justify-center lg:justify-start py-8" href="https://www.google.fr/maps/place/La+Commanderie/@47.0874673,5.4962086,17z/data=!4m5!3m4!1s0x478d4c9ca8cd6cad:0x6fa3daacbb490b3c!8m2!3d47.0873564!4d5.4975096"><img src="../../public/img/map.webp" alt="Carte de l'endroit où le festival aura lieu"></a>
+            <p class="text-sm lg:text-base">Si vous souhaitez prendre connaissance des environs, cliquez sur la carte pour vous rendre sur la localisation de Google Maps.</p>
+            
+            <h4 class="font-semibold text-base lg:text-lg pt-8 pb-6 lg:text-left">Pass et tarifs</h4>
+            <p class="text-sm lg:text-base">Pour accéder aux concerts, différentes formules vous seront proposées durant toute la durée du festival :</p>
+            <ul class="text-sm lg:text-base space-y-4 py-4">
+                <li>Pass Journalier (9€ pièce) : Donne accès au festival pour la journée en cours ;</li>
+                <li>Pass 3 Jours (16€ pièce) : Donne accès au festival pour 3 jours, consécutifs ou non (à faire tamponner à chaque première entrée du jour dans le festival) ;</li>
+                <li>Pass Complet (22€ pièce) : Donne accès au festival pour l'entièreté de sa durée</li>
+            </ul>
+            <p class="text-sm lg:text-base">Il existe également un tarif spécial jeunes (15-25 ans) qui permet, sur présentation de carte Avantage Jeune ou de carte étudiante, de bénéficier d'une remise de 50% sur tous les tarifs.</p>
+        </div>
+    </div>
 </template>
