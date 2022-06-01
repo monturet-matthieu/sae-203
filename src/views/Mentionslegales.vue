6527388e06b4d67913c4162bf11660d4b4f20d0d
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2022-06-01 08:44:00 +0200

    paramétrage mise en ligne

--- src/views/Mentionslegales.vue
+++ src/views/Mentionslegales.vue
@@ -13,13 +13,13 @@
           <h3 class="font-semibold text-xl text-center pt-16 pb-8 lg:text-left">Conditions d’utilisation</h3>
           <p>Ce site a été codé grâce à plusieurs langages web (HTML5, Javascript, CSS). Pour une meilleure expérience utilisateur, nous vous recommandons de recourir à des navigateurs modernes comme Google Chrome, FireFox…</p>
           <p>Matthieu Monturet s’assure que le site émette des informations fiables et veillent à ce qu’il soit mis à jour. Toutefois, des erreurs peuvent survenir. L’utilisateur aura la possibilité de vérifier l'exactitude des informations et pourra les signaler au propriétaire du site s’il le juge utile.</p>
-          <p>Cookies : Le site <a class="text-indigo-400" href="/">bluejazz.feminin.eu</a> sera peut-être amené à vous demander l’acceptation des cookies. Un cookie est un petit fichier stocké par un serveur dans le terminal (ordinateur, téléphone, etc.) d'un utilisateur et associé à un domaine web.</p>
+          <p>Cookies : Le site <a class="text-indigo-400" href="http://bluejazz.femin-in.eu">bluejazz.femin-in.eu</a> sera peut-être amené à vous demander l’acceptation des cookies. Un cookie est un petit fichier stocké par un serveur dans le terminal (ordinateur, téléphone, etc.) d'un utilisateur et associé à un domaine web.</p>
           <p>Liens externes :  Les sites internet peuvent avoir des liens menant vers d’autres sites internet ou d’autres ressources disponibles sur Internet. Matthieu Monturet ne dispose pas de moyens lui permettant de modifier le contenu d’un site dont il n'est pas l’éditeur. Il ne peut donc pas être tenu responsable de tout incident qui pourrait survenir. Les risques, aussi minimes peuvent-ils être, sont à la charge de l’utilisateur.</p>
           <p>Les utilisateurs possédant un site internet ne peuvent pas mettre le lien de ce site sans avoir eu les autorisations de Matthieu Monturet. Dans le cas où l'utilisateur souhaite avoir cette autorisation, il lui suffira de faire une demande sur le mail <a class="text-indigo-400" href=mailto:bluejazzfestival39@gmail.com>bluejazzfestival39@gmail.com</a></p>
           <p>Matthieu Monturet aura alors le droit d’accepter ou non sans se justifier.</p>
         
           <h4 class="font-semibold text-lg text-center pt-8 pb-6 lg:text-left">Services fournis</h4>
-          <p>L'ensemble des activités ainsi que ses informations sont présentés sur le site <a class="text-indigo-400" href="/">bluejazz.feminin.eu</a>.</p>
+          <p>L'ensemble des activités ainsi que ses informations sont présentés sur le site <a class="text-indigo-400" href="http://bluejazz.femin-in.eu/">bluejazz.femin-in.eu</a>.</p>
           <p>Les informations données sur ce site sont à titre indicatif et peuvent changer dans le futur. Les renseignements ne sont pas exhaustif et les illustrations sont non-contractuelles. Toutes les illustrations viennent de banques d’illustrations qui sont libres de droit.</p>
           
           <h4 class="font-semibold text-lg text-center pt-8 pb-6 lg:text-left">Limitation contractuelles sur les données</h4>
@@ -33,10 +33,10 @@
           <p>Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l'égard des traitements de données à caractère personnel), ce site n'a pas encore fait l'objet d'une déclaration auprès de la CNIL.</p>
         
           <h4 class="font-semibold text-lg text-center pt-8 pb-6 lg:text-left">Litiges</h4>
-          <p>Le site <a class="text-indigo-400" href="/">bluejazz.feminin.eu</a> est régie par les lois françaises et toute contestation ou litiges qui pourraient naître seront de la compétence exclusive des tribunaux, la langue de référence étant le français.</p>
+          <p>Le site <a class="text-indigo-400" href="http://bluejazz.femin-in.eu/">bluejazz.femin-in.eu</a> est régie par les lois françaises et toute contestation ou litiges qui pourraient naître seront de la compétence exclusive des tribunaux, la langue de référence étant le français.</p>
         
           <h3 class="font-semibold text-xl text-center pt-16 pb-8 lg:text-left">Données personnelles</h3>
-          <p>Vous n’êtes techniquement pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet <a class="text-indigo-400" href="/">bluejazz.feminin.eu</a>.</p>
+          <p>Vous n’êtes techniquement pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet <a class="text-indigo-400" href="http://bluejazz.femin-in.eu/">bluejazz.femin-in.eu</a>.</p>
           <p>Mais, pour accéder à la plupart des services de notre site, vous allez être amené à nous communiquer un email. D’autres informations pourront vous être éventuellement demandées. C’est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique «Se préinscrire gratuitement ». Si vous ne fournissez pas d’adresse mail, vous ne pourrez pas avoir de renseignement sur nos prochaines activités ou recevoir des informations exclusives</p>
           <p>Nous avons la possibilité de recueillir des informations qui concernent l’utilisation de notre site comme le temps que vous passez sur les pages, les zones où vous cliquez le plus, le type de navigateur que vous utilisez… Ces informations feront l’objet de statistiques au sein des membres de l’équipe. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.</p>
         </div>
