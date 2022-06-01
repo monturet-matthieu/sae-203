f7a54835c409ef1cfc061d169253bf5d64b36e64
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2022-06-01 09:03:50 +0200

    correction liens footer + maj composant form

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="flex justify-center pt-4">
         <ul class="flex flex-col space-y-4 text-center mb-4">
-            <li><a href="contact">Contact</a></li>
+            <li><RouterLink to="/contact">Contact</RouterLink></li>
             <div class="flex space-x-8">
             <li><a href="https://www.instagram.com/matthieumonturet/"><img class="w-8 h-8" src="../../../public/icons/instagram.svg" alt="Logo Instagram"></a></li>
             <li><a href="https://twitter.com/MatthieuMontur1"><img class="w-8 h-8" src="../../../public/icons/twitter.svg" alt="Logo Twitter"></a></li>
@@ -9,7 +9,7 @@
             </div>
         </ul>
     </nav>
-    <h4 class="flex justify-around pb-4"><a href="mentionslegales">Mentions légales</a></h4>
+    <RouterLink class="flex justify-around pb-4" to="/mentionslegales">Mentions légales</RouterLink>>
 </template>
 
 <script>
