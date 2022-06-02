8a256a670cad2dc30183b547eb200089b5813fe6
monturet-matthieu <matthieu.monturet@edu.univ-fcomte.fr>
2022-06-02 08:45:26 +0200

    changement mise en forme composants

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,103 +1,91 @@
 <template>
-  <div class="mb-4">
-    <div class="flex justify-center md:space-x-16 xl:space-x-40 xl:mx-64 sm:mx-8 sm:flex-col md:flex-row md:mx-16">
+    <div class="grid grid-cols-1 mx-8 md:grid-cols-2 md:gap-20 md:mx-12 lg:mx-24 xl:gap-28 2xl:mx-40">
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/ncy.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">NCY Milky Band</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/maalouf.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Ibrahim Maalouf</h3>
             </div>
         </RouterLink>
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
                 <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/recre.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">La Récré</h3>
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">La Récré</h3>
             </div>
         </RouterLink>
     </div>
-  </div>
 
-  <div class="mb-4">
-    <div class="flex justify-center md:space-x-16 xl:space-x-40 xl:mx-64 sm:mx-8 sm:flex-col md:flex-row md:mx-16 lg:space-x-40">
+    <div class="grid grid-cols-1 mx-8 md:grid-cols-2 md:gap-20 md:mx-12 lg:mx-24 xl:gap-28 2xl:mx-40">
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full lg:shrink-0" src="../../public/img/badault.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Denis Badault</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/ncy.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">NCY Milky Band</h3>
             </div>
         </RouterLink>
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full" src="../../public/img/mains-sales.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Les Mains Sales</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/gitanes-mais.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Les Gitanes Maïs</h3>
             </div>
         </RouterLink>
     </div>
-  </div>
 
-  <div class="mb-4">
-    <div class="flex justify-center md:space-x-20 xl:space-x-40 xl:mx-64 sm:mx-8 sm:flex-col md:flex-row md:mx-16 lg:space-x-40">
+    <div class="grid grid-cols-1 mx-8 md:grid-cols-2 md:gap-20 md:mx-12 lg:mx-24 xl:gap-28 2xl:mx-40">
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full" src="../../public/img/gitanes-mais.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Les Gitanes Maïs</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/badault.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Denis Badault</h3>
             </div>
         </RouterLink>
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full" src="../../public/img/emile-parisien.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Emile Parisien</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/mains-sales.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Les Mains Sales</h3>
             </div>
         </RouterLink>
     </div>
-  </div>
 
-  <div class="mb-4">
-    <div class="flex justify-center md:space-x-24 xl:space-x-40 xl:mx-64 sm:mx-8 sm:flex-col md:flex-row md:mx-16 lg:space-x-40">
+    <div class="grid grid-cols-1 mx-8 md:grid-cols-2 md:gap-20 md:mx-12 lg:mx-24 xl:gap-28 2xl:mx-40">
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-56 lg:h-72 mb-6 w-full" src="../../public/img/maalouf.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Ibrahim Maalouf</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/emile-parisien.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Emile Parisien</h3>
             </div>
         </RouterLink>
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-56 lg:h-72 mb-6 w-full" src="../../public/img/vallejo.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Gabriel Vallejo</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/stabar.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Stabar</h3>
             </div>
         </RouterLink>
     </div>
-  </div>
 
-  <div class="mb-4">
-    <div class="flex justify-center md:space-x-32 xl:space-x-64 xl:mx-64 sm:mx-8 sm:flex-col md:flex-row md:mx-16 lg:space-x-64">
+    <div class="grid grid-cols-1 mx-8 md:grid-cols-2 md:gap-20 md:mx-12 lg:mx-24 xl:gap-28 2xl:mx-40">
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full" src="../../public/img/orchestre.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8 mx-2 md:max-w-xs">Orchestre Régional de Jazz d'Alsace</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/vallejo.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Gabriel Vallejo</h3>
             </div>
         </RouterLink>
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full" src="../../public/img/stabar.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Stabar</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/orchestre.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Orchestre Jazz Alsace</h3>
             </div>
         </RouterLink>
     </div>
-  </div>
 
-  <div class="mb-12">
-    <div class="flex justify-center md:space-x-16 xl:space-x-40 xl:mx-64 sm:mx-8 sm:flex-col md:flex-row md:mx-16">
+    <div class="grid grid-cols-1 mx-8 md:grid-cols-2 md:gap-20 md:mx-12 lg:mx-24 xl:gap-28 2xl:mx-40">
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full" src="../../public/img/baptiste-herbin.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Baptiste Herbin</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/baptiste-herbin.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Baptiste Herbin</h3>
             </div>
         </RouterLink>
         <RouterLink to="/artiste">
-            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-24">
-                <img class="rounded-t-3xl xl:h-72 md:h-48 lg:h-72 mb-6 w-full" src="../../public/img/quartet.webp" alt="Banderolle d'illustration">
-                <h3 class="text-2xl font-bold font-montserrat text-gray-100 mb-8">Maxime Bender Quartet</h3>
+            <div class="flex flex-col text-center bg-blue-850 rounded-t-xl rounded-b-md sm:mb-12">
+                <img class="rounded-t-3xl xl:h-72 md:h-36 lg:h-48 mb-6 w-full" src="../../public/img/quartet.webp" alt="Banderolle d'illustration">
+                <h3 class="text-lg md:text-xl lg:text-2xl font-bold font-montserrat text-gray-100 mb-8">Maxime Bender Quartet</h3>
             </div>
         </RouterLink>
     </div>
-  </div>
 </template>
